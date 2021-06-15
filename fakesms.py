44e94481ac2fa12a09635f1a0a35f0e6b67df3e7
X-HACKRAWI <49913818+X-HACKRAWI@users.noreply.github.com>
2021-06-15 16:06:35 -0400

    Update fakesms.py

--- fakesms.py
+++ fakesms.py
@@ -16,6 +16,17 @@ except ImportError:
     print("Error to import 'requests' Library\ntodo : python3 -m pip install requests")
     exit()
 
+# colors
+yellow='\033[93m'
+gren='\033[92m'
+cyan='\033[96m'
+pink='\033[95m'
+red='\033[91m'
+b='\033[1m'
+W = '\033[0m'
+colors = ['\033[1;31m', '\033[1;32m', '\033[1;33m', '\033[1;34m', '\033[1;35m', '\033[1;36m']
+
+
 
 def clr():
 	if os.name == 'nt':
@@ -26,7 +37,7 @@ def clr():
 def banner():
     clr()
     logo = """                                                  
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+\033[96m++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
  ##::::'##:::::::::::'######::'##::::'##::'######::
 . ##::'##:::::::::::'##... ##: ###::'###:'##... ##:
 :. ##'##:::::::::::: ##:::..:: ####'####: ##:::..::
@@ -35,15 +46,15 @@ def banner():
 : ##:. ##:::::::::::'##::: ##: ##:.:: ##:'##::: ##:
  ##:::. ##::::::::::. ######:: ##:::: ##:. ######::
 ..:::::..::::::::::::......:::..:::::..:::......:::  
-     WhatsApp>  X-HACKRAWI    +31 97005033396"     version = V2       
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  
+    \033[91mX-HACKRAWI   WhatsApp >     \033[95m+31 97005033396      \033[96mv2.1      
+\033[96m++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  
                                          """
     print(logo)
     print("\n")
     
 
 def Track() :
-  TXTID = input("Enter Text ID of X-SMS \n\t -->>")
+  TXTID = input("\033[96mEnter Text ID of X-SMS \n\t -->>")
   os.system(f"curl https://textbelt.com/status/{TXTID}")
   input("\nPress Enter To Exit..")
   print("\nThanks For Using X-SMS..")
@@ -51,9 +62,51 @@ def Track() :
   print("Thank you for Your Time...")
   exit()
 
+def update():
+    stuff_to_update = ['fakesms.py', 'Run.sh', '.version']
+    for fl in stuff_to_update:
+        dat = urllib.request.urlopen("https://raw.githubusercontent.com/X-HACKRAWI/FreeSms/master/" + fl).read()
+        file = open(fl, 'wb')
+        file.write(dat)
+        file.close()
+    print('\n\t\tUpdated Successfull !!!!')
+    print('\tRun The Script Again...')
+    exit()
+
+
+
 clr()
 banner()
-   
+
+try:
+    urllib.request.urlopen('https://www.google.com')
+except Exception:
+    print("Error While Connecting To Internet!!!")
+    print("\tPlease Connect To Internet To Continue...\n")
+    input('Exiting....\n Press Enter To Exit....')
+    exit()
+print('\tChecking For Updates...')
+ver = urllib.request.urlopen("https://raw.githubusercontent.com/X-HACKRAWI/FreeSms/master/.version").read().decode('utf-8')
+verl = ''
+try:
+    verl = open(".version", 'r').read()
+except Exception:
+    pass
+if ver != verl:
+    print('\n\t\tAn Update is Available....')
+    print('\tUpdating X-SMS...')
+    update()
+print("Congratulation")
+print("Your Version is Up-To-Date")
+print('\n\tStarting X-SMS...\n')
+try:
+    noti = urllib.request.urlopen("https://raw.githubusercontent.com/X-HACKRAWI/FreeSms/master/.notify").read().decode('utf-8')
+    if len(noti) > 10:
+        print('\nNotification : ' + noti + '\n')
+except Exception:
+    pass
+
+
 while True:
 	print("\033[0mThis Tool Is Used To Send Anonymous Messages")
 	break
@@ -70,7 +123,7 @@ if type == 1:
 elif type == 0:
 	while True:
 		print("Enter The Details Of The Person You Want To Send Anonymous Message")
-		cc = input("\tEnter Country Code (Without +) : ")
+		cc = input("\033[96m\tEnter Country Code (Without +) : ")
 		if '+' in cc:
 		        tc = list(cc)
 		        tc.remove('+')
@@ -79,7 +132,7 @@ elif type == 0:
 		if len(cc) >= 4 or len(cc) < 1:
 		        print('\n\nInvalid Country Code..\n\t\tCountry Codes Are Generally 1-3 digits...\n')
 		        continue
-		pn = input("Enter Phone Number : +" + cc + " ")
+		pn = input("\033[96mEnter Phone Number : +" + cc + " ")
 		if len(pn) <= 6:
 		        print('\n\nInvalid Phone Number..\n')
 		        continue
@@ -88,7 +141,7 @@ elif type == 0:
 		            print('\n\nPhone Number Must Consist Of Numbers Only\n')
 		            continue
 		receiver = '+' + numbe
-		text = input("Enter Message to send : ")
+		text = input("\033[96mEnter Message to send : ")
 		
 		resp = requests.post('https://textbelt.com/text',{
 			'phone' : receiver,
